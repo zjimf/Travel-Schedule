8e09d43f54ef5baf0d31461ac30684fd49db4f6e
zjimf <jmjmjim1101@gmail.com>
2023-11-27 13:37:20 +0800

    Change Wrong msg

--- src/Public/Methods/HandleErrMsg.js
+++ src/Public/Methods/HandleErrMsg.js
@@ -1,19 +1,19 @@
 function HandleErrorMsg(msg) {
   switch (msg) {
     case "auth/invalid-email":
-      return "Invalid-email";
+      return "Invalid email";
     case "auth/invalid-password":
-      return "Invalid-password";
+      return "Invalid password";
     case "auth/email-already-in-use":
-      return "Email-already-in-use";
+      return "Email already-in-use";
     case "auth/weak-password":
-      return "Weak-password";
+      return "Weak password";
     case "auth/wrong-password":
-      return "Wrong-password";
+      return "Wrong password";
     case "auth/user-not-found":
-      return "User-not-found";
+      return "User not found";
     case "auth/invalid-login-credentials":
-      return "Invalid-login-credentials";
+      return "Invalid login credentials";
     default:
       return "";
   }
