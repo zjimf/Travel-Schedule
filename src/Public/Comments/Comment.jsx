73714edf65ba6aa6b885e32f1a9dc5112f00f57e
zjimf <jmjmjim1101@gmail.com>
2023-12-20 21:29:51 +0800

    delete comment fucntion

--- src/Public/Comments/Comment.jsx
+++ src/Public/Comments/Comment.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
 import { FormatDate } from "../Methods/FormateDate";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { DeleteMsg } from "../Database/DeleteMsg";
 import { GetUID } from "../../Public/Database/GetUID";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Comment = ({ comment, docID, index }) => {
   const [uid, setUid] = useState("");
   const [deleted, setDeleted] = useState(false);
-
+  const [deleting, setDeleting] = useState(false);
   const handleDelete = () => {
+    setDeleting(true);
     DeleteMsg(comment, docID, index, setDeleted);
   };
 
@@ -20,37 +23,65 @@ const Comment = ({ comment, docID, index }) => {
     }
     get();
   }, [uid]);
+
   return (
     <>
       {deleted || (
-        <Grid container wrap="nowrap" spacing={2} sx={{ marginX: "20px" }}>
-          <Grid item>
-            <Avatar
-              alt="Remy Sharp"
-              src={
-                comment.avatar === "" || comment.avatar === undefined
-                  ? ""
-                  : require(`../../Public/Images/avatar/avatar${comment.avatar}.png`)
-              }
-            />
-          </Grid>
+        <Box
+          sx={{
+            marginX: "30px",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Avatar
+                alt="Remy Sharp"
+                src={
+                  comment.avatar === "" || comment.avatar === undefined
+                    ? ""
+                    : require(`../../Public/Images/avatar/avatar${comment.avatar}.png`)
+                }
+              />
+              <h4 style={{ margin: "0 0 0 10px", textAlign: "center" }}>
+                {comment.name}
+              </h4>
+            </Box>
+            <Box sx={{ marginLeft: "50px" }}>
+              <p style={{ textAlign: "left" }}>{comment.comment}</p>
+              <p style={{ textAlign: "left", color: "gray" }}>
+                {FormatDate(comment.timeStamp.seconds * 1000)}
+              </p>
+            </Box>
+          </Box>
 
-          <Grid justifyContent="left" item zeroMinWidth>
-            <h4 style={{ margin: 0, textAlign: "left" }}>{comment.name}</h4>
-            <p style={{ textAlign: "left" }}>{comment.comment}</p>
-            <p style={{ textAlign: "left", color: "gray" }}>
-              {FormatDate(comment.timeStamp.seconds * 1000)}
-            </p>
-          </Grid>
-          {comment.uid === uid ? (
-            <DeleteForeverIcon
-              sx={{ cursor: "pointer", fontSize: "30px !important" }}
-              onClick={handleDelete}
-            />
-          ) : (
-            <></>
-          )}
-        </Grid>
+          <Box>
+            {comment.uid !== uid ||
+              (deleting ? (
+                <CircularProgress />
+              ) : (
+                <DeleteForeverIcon
+                  sx={{ cursor: "pointer", fontSize: "30px !important" }}
+                  onClick={handleDelete}
+                />
+              ))}
+          </Box>
+        </Box>
       )}
     </>
   );
